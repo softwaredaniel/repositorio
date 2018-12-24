34a29d00f1a59d62b11d3ce704ff9ef50f707f08
softwaredaniel <36455248+softwaredaniel@users.noreply.github.com>
2018-12-24 10:01:02 -0500

    Delete Cerrar.php

--- Cerrar.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	session_start();//SE INICIA SESSION 
-
-	unset($_SESSION['user']);//SE VACIA LA SESSION 
-
-	header("location:../Requerimientos/Index.html");// SE REDIRIGE A LA PAGINA QUE NOSOTROS QUERAMOS
-
- ?>
